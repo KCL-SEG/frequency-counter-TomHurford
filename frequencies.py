1b778fe01ad5cd00cb802bcfcdd06954c88437f2
Tom Hurford <hurford.t0@gmail.com>
2022-10-01 16:11:15 +0100

    011022 - TGH - Code working

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+# Pass a list of items to the function convert the list to a dictionary with the frequency of each item in the list convert all items in the list to strings
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for item in items:
+        if str(item) in frequencies:
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1
+            
     return frequencies
